163810a05f27bbd195000727156512405766702b
Carcharodon80 <romanakulov1980@gmail.com>
2024-07-02 00:37:48 +0300

    1. RESTFul. Описание архитектуры [#6884]

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -1,6 +1,57 @@
 package ru.job4j.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.domain.Person;
+import ru.job4j.repository.PersonRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/person")
 public class PersonController {
+    private final PersonRepository persons;
+
+    public PersonController(PersonRepository persons) {
+        this.persons = persons;
+    }
+
+    @GetMapping("/")
+    public List<Person> findAll() {
+        return (List<Person>) persons.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable int id) {
+        Optional<Person> person = persons.findById(id);
+        return new ResponseEntity<>(
+                person.orElse(new Person()),
+                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Person> create(@RequestBody Person person) {
+        return new ResponseEntity<>(
+                persons.save(person),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Void> update(@RequestBody Person person) {
+        persons.save(person);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        Person person = new Person();
+        person.setId(id);
+        persons.delete(person);
+        return ResponseEntity.ok().build();
+    }
 }
 
-//todo расписать контроллер
